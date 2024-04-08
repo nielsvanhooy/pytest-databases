f6e9600346fb354882ddc0de8d57c5770b491713
hooij804 <niels.vanhooij@kpn.com>
2024-04-08 20:25:04 +0200

    added motor test database. i had this in my litestar fullstack repo. but they craeted a new repo with test databases very nice. so i added it back in here.

---
+++ src/pytest_databases/docker/mongodb.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import os
+from typing import TYPE_CHECKING
+import pytest
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.errors import ServerSelectionTimeoutError
+
+
+async def mongo_responsive(host: str, mongo_url: str) -> bool:
+    try:
+        client: AsyncIOMotorClient = AsyncIOMotorClient(mongo_url)
+        return bool(await client.server_info())
+    except ServerSelectionTimeoutError:
+        return False
+
+if TYPE_CHECKING:
+    from pytest_databases.docker import DockerServiceRegistry
+
+
+@pytest.fixture()
+def mongo_db_user() -> str:
+    return "pytest"
+
+
+@pytest.fixture()
+def mongo_db_password() -> str:
+    return "pytest"
+
+
+@pytest.fixture()
+def mongo_db_port() -> str:
+    return "17017"
+
+
+@pytest.fixture()
+def mongo_url(mongo_db_user: str, mongo_db_password: str, mongo_db_port: str) -> str:
+    return f"mongodb://{mongo_db_user}:{mongo_db_password}@localhost:{mongo_db_port}"
+
+
+@pytest.fixture()
+async def mongo_service(docker_services: DockerServiceRegistry, mongo_db_port: str, mongo_url: str) -> None:
+    os.environ["MONGO_PORT"] = str(mongo_db_port)
+    await docker_services.start("mongo", check=mongo_responsive, mongo_url=mongo_url)
